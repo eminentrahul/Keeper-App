8f211a1391fb1747327aeddbd8bfcb545bd99a28
Rahul Ravi Prakash <eminentrahul@rocketmail.com>
2020-02-11 00:09:54 +0530

    Sample data added.

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import NoteData from "../Notes";
+
+console.log(NoteData);
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {NoteData.map(props => (
+        <Note key={props.key} title={props.title} content={props.content} />
+      ))}
       <Footer />
     </div>
   );
---
+++ src/Notes.js
@@ -0,0 +1,30 @@
+const notes = [
+  {
+    key: 1,
+    title: "First Note",
+    content: "This is the content for first note."
+  },
+  {
+    key: 2,
+    title: "Second Note",
+    content:
+      "This is the content for first note.This is the content for first note.This is the content for first note.This is the content for first note."
+  },
+  {
+    key: 3,
+    title: "Third Note",
+    content: "This is the content for first note."
+  },
+  {
+    key: 4,
+    title: "Fourth Note",
+    content: "This is the content for first note."
+  },
+  {
+    key: 5,
+    title: "Fifth Note",
+    content: "This is the content for first note."
+  }
+];
+
+export default notes;
